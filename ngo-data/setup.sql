6222247e97be772e791a7d9dd447bb569c52f74d
Liam <liam.jb1@gmail.com>
2018-07-22 09:04:00 +1200

    New table definition

--- ngo-data/setup.sql
+++ ngo-data/setup.sql
@@ -5,3 +5,12 @@ CREATE TABLE RESOURCES(
   Description VARCHAR(50),
   CONSTRAINT resources_pkey PRIMARY KEY(Organisation, Resource)
 );
+
+CREATE TABLE LOCATIONS(
+  Organisation VARCHAR(50) NOT NULL,
+  Resource VARCHAR(50) NOT NULL,
+  Quantity INT DEFAULT 0,
+  Location VARCHAR(50) NOT NULL,
+  CONSTRAINT locations_pkey PRIMARY KEY(Organisation, Resource)
+  CONSTRAINT locations_fkey FOREIGN KEY(Organisation, Resource) REFERENCES RESOURCES
+);
